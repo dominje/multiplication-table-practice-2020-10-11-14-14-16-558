d1e37d8a1b36242ae7c368eb12a3491c6a6c2e42
dominje <jericah.domingo@oocl.com>
2020-10-16 17:51:20 +0800

    [MultiplicationTable][jericah]feature: added logic for multiplication table

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -2,6 +2,32 @@ package com.tw;
 
 public class MultiplicationTable {
     public String create(int startNumber, int endNumber) {
-        return null;
+        boolean isValid = checkIfValid(startNumber, endNumber);
+        String multiplicationTable = generateMultiplicationTable(startNumber, endNumber);
+        return isValid ? multiplicationTable : null;
+    }
+
+    public static boolean checkIfValid(int startNum, int endNum){
+        boolean isNumberValid = checkNumberValidity(startNum, endNum);
+        boolean isStartNumberInRange = checkIfInRange(startNum);
+        boolean isEndNumberInRange = checkIfInRange(endNum);
+        return isNumberValid && isStartNumberInRange && isEndNumberInRange;
+    }
+
+
+    public static boolean checkIfInRange(int number) {
+        return number > 0 && number < 1000;
+    }
+
+    public static boolean checkNumberValidity(int startNum, int endNum) {
+        return startNum<=endNum;
+    }
+
+    public static String generateMultiplicationTable(int startNum, int endNum) {
+        String multiplicationTable = "";
+        for (int i=startNum; i<=endNum; i++){
+            multiplicationTable = (startNum + "*" + i + "=" + (startNum*i));
+        }
+        return multiplicationTable;
     }
 }
